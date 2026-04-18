a4957e8ae865864481d15133e8e847eac28d4dd8
HamzaAzeem99 <muhammadhamzamuhammadazeem@gmail.com>
2026-04-19 04:40:10 +0500

    some code

---
+++ app.js
@@ -0,0 +1,9 @@
+let main = document.getElementById("main")
+let loader = document.getElementById("🤚")
+let load = document.getElementById("load")
+
+setTimeout(() => {
+    loader.style.display = "none"
+    load.style.display = "none"
+    main.style.display = "block"
+}, 3000);
